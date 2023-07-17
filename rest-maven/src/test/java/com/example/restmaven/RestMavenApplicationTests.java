9a8a9745bb38af128a955db468566df85d38dac3
Schmidt Robert <robstoner19@gmail.com>
2023-07-17 14:04:55 +0300

    First commit

---
+++ rest-maven/src/test/java/com/example/restmaven/RestMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
