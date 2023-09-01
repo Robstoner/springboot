7c3c6e903335130a33afd1d904a3f874a28a12ad
Schmidt Robert <robstoner19@gmail.com>
2023-09-01 16:07:09 +0300

    Oauth2 technically works, don't know how to implement it fully

--- real-test/src/main/java/com/test/realworldexample/config/SecurityConfig.java
+++ real-test/src/main/java/com/test/realworldexample/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,14 +22,17 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthFilter;
     @Autowired
     private final AuthenticationProvider authenticationProvider;
-    
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.disable())
-            .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/auth/**").permitAll().anyRequest().authenticated())
-            .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        http
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .oauth2Login(Customizer.withDefaults())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
